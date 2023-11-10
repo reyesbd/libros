d1587fe45b40fb7f3c0545f430a7032f01b86672
liliasoto <0.lilia.soto.0@gmail.com>
2023-11-10 11:43:31 -0600

    Se insertaron datos en la base de datos

--- database.sql
+++ database.sql
@@ -33,3 +33,55 @@ CREATE TABLE libros(
     FOREIGN KEY (fk_id_editorial)   REFERENCES editoriales(id_editorial),
     FOREIGN KEY (fk_id_tema)        REFERENCES temas(id_tema)
 );
+
+-- Insertar autores
+INSERT INTO autores (nombre, apellidos) VALUES
+('Gabriel', 'García Márquez'),
+('J.K.', 'Rowling'),
+('George', 'Orwell'),
+('Harper', 'Lee'),
+('J.R.R.', 'Tolkien'),
+('Agatha', 'Christie'),
+('Ernest', 'Hemingway'),
+('Jane', 'Austen'),
+('F. Scott', 'Fitzgerald'),
+('Leo', 'Tolstoy');
+
+-- Insertar editoriales
+INSERT INTO editoriales (nombre) VALUES
+('Random House'),
+('Bloomsbury'),
+('Penguin Books'),
+('HarperCollins'),
+('Houghton Mifflin Harcourt'),
+('Agatha Christie Ltd.'),
+('Scribner'),
+('Penguin Classics'),
+('Scribner'),
+('Vintage Classics');
+
+-- Insertar temas
+INSERT INTO temas (nombre) VALUES
+('Ficción'),
+('Fantasía'),
+('Distopía'),
+('Clásico'),
+('Misterio'),
+('Novela Romántica'),
+('Novela Histórica'),
+('Novela Contemporánea'),
+('Policíaca'),
+('Épica');
+
+-- Insertar libros
+INSERT INTO libros (titulo, edicion, fk_id_autor, fk_id_editorial, fk_id_tema) VALUES
+('Cien años de soledad', '1ª edición', 1, 1, 1),
+('Harry Potter y la piedra filosofal', '1ª edición', 2, 2, 2),
+('1984', '1ª edición', 3, 3, 3),
+('To Kill a Mockingbird', '1st Edition', 4, 4, 4),
+('The Hobbit', '1st Edition', 5, 5, 5),
+('Murder on the Orient Express', '1st Edition', 6, 6, 6),
+('The Old Man and the Sea', '1st Edition', 7, 7, 7),
+('Pride and Prejudice', '1st Edition', 8, 8, 8),
+('The Great Gatsby', '1st Edition', 9, 9, 9),
+('War and Peace', '1st Edition', 10, 10, 10);
